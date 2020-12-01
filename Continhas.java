515c887284f6de9b8a557b6f622eb544b2c75799
professorisidro <isidro@professorisidro.com.br>
2020-12-01 12:25:52 -0300

    primeiro commit

---
+++ Continhas.java
@@ -0,0 +1,26 @@
+public class Continhas{
+    public static void main(String args[]){
+        int a,b,c;
+
+        a = 11;
+        b = 4;
+
+        c = a + b;
+        System.out.println("Valor da soma = " + c);
+        c = a - b;
+        System.out.println("Valor da subtracao = " + c);
+        c = a * b;
+        System.out.println("Valor da multiplicacao =  " + c);
+        c = a / b;
+        System.out.println("Valor da divisao = " + c);
+        c = a % b;
+        System.out.println("Valor do resto = " + c);
+
+        float f;
+        f = 11/(float)b;
+
+        System.out.println("Valor da divisao quebrada = " + f);
+
+
+    }
+}
